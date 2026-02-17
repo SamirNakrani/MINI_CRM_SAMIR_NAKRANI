8552b4feb58ed91e4d2fe2ee93f4f3fbc029124f
SamirNakrani <samirna@andaze.com>
2026-02-17 19:14:16 +0530

    Add project files.

---
+++ MINI_CRM_SAMIR_NAKRANI.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,57 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Xpo;
+using MINI_CRM_SAMIR_NAKRANI.Blazor.Server.Services;
+
+namespace MINI_CRM_SAMIR_NAKRANI.Blazor.Server
+{
+    public class MINI_CRM_SAMIR_NAKRANIBlazorApplication : BlazorApplication
+    {
+        public MINI_CRM_SAMIR_NAKRANIBlazorApplication()
+        {
+            ApplicationName = "MINI_CRM_SAMIR_NAKRANI";
+            CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            DatabaseVersionMismatch += MINI_CRM_SAMIR_NAKRANIBlazorApplication_DatabaseVersionMismatch;
+        }
+        protected override void OnSetupStarted()
+        {
+            base.OnSetupStarted();
+
+#if DEBUG
+            if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+            }
+#endif
+        }
+        void MINI_CRM_SAMIR_NAKRANIBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e)
+        {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else
+            {
+                string message = "The application cannot connect to the specified database, " +
+                    "because the database doesn't exist, its version is older " +
+                    "than that of the application or its schema does not match " +
+                    "the ORM data model structure. To avoid this error, use one " +
+                    "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+                if (e.CompatibilityError != null && e.CompatibilityError.Exception != null)
+                {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+    }
+}
