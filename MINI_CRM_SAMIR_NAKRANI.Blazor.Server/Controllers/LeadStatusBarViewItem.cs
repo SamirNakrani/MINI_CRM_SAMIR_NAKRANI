e6b4a857cb71b1e4f73bd4eb5956cb6ae4c0f1a2
SamirNakrani <samirna@andaze.com>
2026-02-26 13:00:28 +0530

    feature/10: Implemented State Machine

---
+++ MINI_CRM_SAMIR_NAKRANI.Blazor.Server/Controllers/LeadStatusBarViewItem.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+using Microsoft.AspNetCore.Components;
+using MINI_CRM_SAMIR_NAKRANI.Module.BusinessObjects;
+
+namespace MINI_CRM_SAMIR_NAKRANI.Blazor.Server.Controllers
+{
+    [ViewItem(typeof(IModelViewItem))]
+    public class LeadStatusBarViewItem : ViewItem, IComponentContentHolder
+    {
+        public ProcessProgress ComponentInstance { get; private set; }
+
+        public LeadStatusBarViewItem(IModelViewItem model, Type objectType) : base(objectType, model.Id) { }
+
+        protected override object CreateControlCore() => this;
+
+        public override void Refresh()
+        {
+            base.Refresh();
+            ComponentInstance?.RefreshUI();
+        }
+
+        RenderFragment IComponentContentHolder.ComponentContent => builder =>
+        {
+
+            var currentObject = View?.CurrentObject;
+
+            if (currentObject is Lead)
+            {
+                builder.OpenComponent<ProcessProgress>(0);
+                builder.AddAttribute(1, nameof(ProcessProgress.ViewItem), this);
+
+                builder.AddComponentReferenceCapture(2, inst => ComponentInstance = (ProcessProgress)inst);
+
+                builder.CloseComponent();
+            }
+        };
+    }
+}
---
+++ MINI_CRM_SAMIR_NAKRANI.Module/BusinessObjects/ProcessStateMachine.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.StateMachine;
+using DevExpress.ExpressApp.StateMachine.NonPersistent;
+using DevExpress.XtraRichEdit.Import.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINI_CRM_SAMIR_NAKRANI.Module.BusinessObjects
+{
+    internal class ProcessStateMachine : StateMachine<Lead>, IStateMachineUISettings
+    {
+
+        private IState startState;
+
+        public ProcessStateMachine(IObjectSpace objectSpace) : base(objectSpace)
+        {
+            startState = new State(this, "Open", ProcessState.Open);
+            IState qualified = new State(this, "Qualified", ProcessState.Qualified);
+            IState proposed = new State(this, "Propose", ProcessState.Propose);
+            IState developed = new State(this, "Develop", ProcessState.Developed);
+            IState closed = new State(this, "Closed", ProcessState.Closed);
+
+            startState.Transitions.Add(new Transition(qualified));
+            qualified.Transitions.Add(new Transition(developed));
+            qualified.Transitions.Add(new Transition(startState));
+            developed.Transitions.Add(new Transition(proposed));
+            developed.Transitions.Add(new Transition(qualified));
+            proposed.Transitions.Add(new Transition(closed));
+            proposed.Transitions.Add(new Transition(developed));
+            closed.Transitions.Add(new Transition(proposed));
+
+            States.Add(startState);
+            States.Add(qualified);
+            States.Add(developed);
+            States.Add(proposed);
+            States.Add(closed);
+        }
+
+        public override string Name => "Change Lead Status";
+        public override string StatePropertyName => nameof(Lead.State);
+        public override IState StartState => startState;
+
+        public bool ExpandActionsInDetailView => false;
+    }
+}
