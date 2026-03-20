1f71850f8363bc9ce824f182bfe08c9c9f742b7b
SamirNakrani <samirna@andaze.com>
2026-03-20 14:47:10 +0530

    feature/ 12: made  Login FIelds Read Only

---
+++ MINI_CRM_SAMIR_NAKRANI.Blazor.Server/Controllers/LoginHistoryReadOnlyActionsController.cs
@@ -0,0 +1,31 @@
+﻿    using DevExpress.ExpressApp;
+    using DevExpress.ExpressApp.Actions;
+    using DevExpress.ExpressApp.SystemModule;
+    using MINI_CRM_SAMIR_NAKRANI.Module.BusinessObjects;
+
+namespace MINI_CRM_SAMIR_NAKRANI.Blazor.Server.Controllers
+{
+    public class LoginHistoryReadOnlyActionsController : ViewController<ListView>
+    {
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            if (View.ObjectTypeInfo.Type == typeof(LoginHistory))
+            {
+                var newController = Frame.GetController<NewObjectViewController>();
+                var deleteController = Frame.GetController<DeleteObjectsViewController>();
+                var modController = Frame.GetController<ModificationsController>();
+
+                if (newController != null)
+                    newController.NewObjectAction.Active["ReadOnly"] = false;
+
+                if (deleteController != null)
+                    deleteController.DeleteAction.Active["ReadOnly"] = false;
+
+                if (modController != null)
+                    modController.SaveAction.Active["ReadOnly"] = false;
+            }
+        }
+    }
+}
